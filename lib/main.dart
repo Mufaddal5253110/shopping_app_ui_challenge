962bff42bd3790192fd2a33cf1b0e967d5485162
MUFADDAL SHAKIR <60619133+Mufaddal5253110@users.noreply.github.com>
2021-04-12 10:49:59 +0530

    initializing

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app_ui_challenge/registration_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RegistrationPage(),
+    );
+  }
+}
